b019652be551c752cf227b63f404d3bb6b50a691
Onea <onea8906@gmail.com>
2021-10-14 19:36:59 +0900

    chore: modify cors URI

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-  origin: `http://localhost:${process.env.CLIENT_PORT}`,
+  origin: `${process.env.CLIENT_URI}`,
   credentials: true,
 }));
 
